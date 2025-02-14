4398d970f0f69080e3c66fe924a341c9ba45bbc3
nemm22 <nemanja.micovic11@gmail.com>
2025-02-14 10:21:16 +0100

    added getAllTags route

--- src/router/tags.ts
+++ src/router/tags.ts
@@ -1,9 +1,10 @@
-import { getTag, newTag, updateTag, deleteTag} from '../controllers/tags';
+import { getTag, newTag, updateTag, deleteTag, showAllTags} from '../controllers/tags';
 import express from 'express';
 import { isAuthenticated,isOwner,ownsPost } from "../middlewares";
 
 
 export default(router: express.Router) => {
+    router.get('/tags/',isAuthenticated,showAllTags);
     router.get('/tags/:id', isAuthenticated, getTag);
     router.post('/tags/', isAuthenticated, newTag);
     router.patch('/tags/:id', isAuthenticated, updateTag);
